e04327623edeaa57a11399ac462a4b9529d7aa36
JbanzCodingLabz <jacob.r.bananal@outlook.com>
2023-10-22 22:04:00 -0700

    MUI -- Added Extensions Folder, Added MUIExtensions

---
+++ Extensions/MUIExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JbanzCodingLabz.MUI.Extensions
+{
+    public static class MUIExtensions
+    {
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+
+    }
+}
